0ac84baa24840560027ca8848b16f7a476ed96ba
Hasan-446 <hasanmahmudul020@gmail.com>
2022-08-25 18:05:44 +0600

    deploy commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './Style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>  
+    </HashRouter>  
   </React.StrictMode>
 );
 
